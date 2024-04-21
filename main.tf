a38f09795d6bcd93cb0200ce1d9793f05aef10d6
Towobolar <71108083+Towobolar@users.noreply.github.com>
2024-04-21 09:23:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+ # The configuration for the `remote` backend.
+    terraform {
+      backend "remote" {
+        # The name of your Terraform Cloud organization.
+        organization = "Cloud-Platform"
+
+        # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+          name = "abbey-terraform-aws"
+        }
+      }
+    }
+
 provider "aws" {
   region = "eu-west-2"
 }
